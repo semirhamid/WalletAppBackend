ed3d787a1dc9af3a1e35366906c1032be09c511f
Semir Hamid <106813382+semirhamid@users.noreply.github.com>
2024-11-24 01:52:26 +0300

    Background Job Added

---
+++ WalletApp.API/Services/DailyPointsJob.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using WalletApp.Application.Features.Points.Commands;
+
+namespace WalletApp.API.Services;
+
+public class DailyPointsJob : BackgroundService
+{
+    private readonly IMediator _mediator;
+
+    public DailyPointsJob(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var today = DateTime.UtcNow.Date;
+            await _mediator.Send(new CalculateDailyPointsCommand { Date = today }, stoppingToken);
+            await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+        }
+    }
+}
